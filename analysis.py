ce5eb62bbe5469c15d4ac3086f5a89842e03cdc4
ajwani-vanshika <ajwanivanshika@gmail.com>
2025-12-27 02:38:59 +0530

    Add files via upload

--- analysis.py
+++ analysis.py
@@ -29,3 +29,37 @@ df["Marks"].fillna(average_marks,inplace=True)
 
 print("\n Missing Values in Marks after replacement:")
 print(df["Marks"].isnull().sum())
+
+print("Average Marks for Each Course:")
+print(df.groupby("Course")["Marks"].mean())
+
+print("\nStudents Scoring More than 85 Marks:")
+print(df[df["Marks"]>85])
+
+highest_marks = df["Marks"].max()
+lowest_marks = df["Marks"].min()
+print("\nHighest Marks:",highest_marks)
+print("\nLowest Marks", lowest_marks)
+
+print("\nNumber of Students in Each Course:")
+print(df["Course"].value_counts())
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+plt.figure(figsize=(10, 5))
+plt.bar(df["Name"], df["Marks"])
+plt.title("Student Names vs Marks")
+plt.xlabel("Student Name")
+plt.ylabel("Marks")
+plt.xticks(rotation=45)
+plt.tight_layout()
+plt.show()
+
+plt.figure(figsize=(8, 5))
+plt.hist(df["Marks"], bins=5)
+plt.title("Marks Distribution")
+plt.xlabel("Marks")
+plt.ylabel("Number of Students")
+plt.tight_layout()
+plt.show()
